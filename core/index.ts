f2874a960caec7f5bc4b55666387fb4d24e29f4b
yi-tuan <1260320217@qq.com>
2020-09-02 22:56:21 +0800

    chore: 💄上下文无关算法
    Signed-off-by: yi-tuan <1260320217@qq.com>

--- core/index.ts
+++ core/index.ts
@@ -1,6 +1,6 @@
 import { Parser, print } from './parser';
 import { tokenizer } from './tokenizer';
-import { parse } from "@babel/parser";
+// import { parse } from "@babel/parser";
 
-console.log(print(new Parser(tokenizer('1 + 2 + 3 * 6')).parse(), ''));
+console.log(print(new Parser(tokenizer('1 + 2 + 3 * 6 * (3 + 4 * 6)')).parse(), ''));
 // console.log(parse('var a = 1 + 2 + 3;'));
